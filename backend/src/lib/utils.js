0ac5d9cb6a9f7293b7aa2f23c70fb7325b98b4fb
Shubham Kumar <shubhamswaraj471@gmail.com>
2026-08-02 00:26:26 +0530

    Feature updated

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,14 +1,16 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId,res) =>{
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"});
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("jwt",token,{expiresIn:"7d",
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-        sameSite:"strict",
-        secure:process.env.NODE_ENV !== "development" 
-    });
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  });
 
-    return token;
+  return token;
 };
