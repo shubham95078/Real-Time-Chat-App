b309a3e4290b872b2d73db8f0a950438ae335dd9
Shubham Kumar <shubhamswaraj471@gmail.com>
2026-08-01 16:52:16 +0530

    Feature updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,9 +21,15 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(cors(
-    {origin: "http://localhost:5173", credentials: true}
-));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes );
 app.use("/api/messages", messageRoutes );
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,9 +7,13 @@ const app=express();
 const server=http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:5173"]
-    },
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
+    credentials: true,
+  },
 });
 
 export function getReceiverSocketId(userId) {
