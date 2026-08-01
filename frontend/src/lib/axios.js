7e601605e745f0d227ba7553f230614efc2866d7
Shubham Kumar <shubhamswaraj471@gmail.com>
2026-08-02 00:13:50 +0530

    Feature updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : `${import.meta.env.VITE_API_URL}/api`;
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api",
-    withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
-
